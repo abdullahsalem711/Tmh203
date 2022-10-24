078c444822afb1e0a5443906df7ff5a9a387a135
abdullah elali elsalem <abdullahalmadani711@gmail.com>
2022-10-24 13:56:47 +0300

    Tmh203

---
+++ HOMEWORK.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+#The law provided several explanations for many phenomena that occur at the level of the universe and at the level of celestial bodies and planets in various galaxies with their differences. Among the explanations given and presented by this law are the following:
+ #Provide an explanation of the rotational pattern that occurs between planets, stars and supernovae that are on the verge of extinction and disintegration. 
+#Presenting the law for a complete explanation of the columbian attraction that occurs at the level of atomic nuclei and at the level of atomic molecules.
+
+g=6.67430*pow(10,-11)
+m1=int(input("enter the mase one:" ))
+m2=int(input("enter the mase tow:" ))   
+r=int(input("enter distance between centers of the masses"))
+f=g*m1*m2/pow(r,2)
+print( " universal gravitation of this  system is",f)
+
+#Relativity is one of the most important theories that express how energy
+#is converted into mass and vice versa, and this conversion is the basis for the work of nuclear power plants
+#, in addition to the glow and radiation of stars.
+c=300000
+m=int(input("enter the mase :" ))
+E=m*pow(c,2)
+print ("the value of energy is",E)
+# Kinetic energy is a type of energy possessed by a body due to its motion, 
+#equal to the work required to accelerate an object from rest to a certain speed, 
+#whether it is a straight or angular velocity.
+a=0.5
+M=int(input("enter the mase:"))
+v=int(input("enter the  speed:"))
+KE=a*M*pow(v,2)
+print("the kinetic energy is:",KE)
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
